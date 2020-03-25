364493d7298322b3c80315adcbe58e593d52a065
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-03-25 00:34:14 -0400

    Cleaned up output comments

--- video-joiner.sh
+++ video-joiner.sh
@@ -35,7 +35,7 @@ if [ $has_ffmpeg -eq 0 ] || [ $has_ffprobe -eq 0 ] ; then
 fi;
 
 ## loop through videos to join ##
-read -e -r -p "Join how many videos? (1) " total_files
+read -e -r -p "Join how many video files? (1) " total_files
 if [ -z $total_files ]; then
   total_files=1;
 fi
@@ -45,24 +45,24 @@ file_idx=1
 while [ $file_idx -le $total_files ]; do
    echo "..........................."
    
-   read -e -r -p "$file_idx: What is the video? " filename
+   read -e -r -p "$file_idx: Enter the video file: " filename
    file_ext=`echo "${filename##*.}" | tr '[:upper:]' '[:lower:]'`
    resolution=`ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 ${filename}`
    duration_time=`ffprobe ${filename} 2>&1 | grep -E '^ +Duration' | cut -d':' -f2- | cut -d, -f1- | cut -d'.' -f1`
    if [ -z $duration_time ]; then
       echo "ERROR: Sorry ${filename} an invalid video file. Quitting."
-      exit;
+      exit
    fi;
 
    read -e -r -p "   .. info: ${resolution}, ${file_ext}, duration: ${duration_time} -- start at (default 00:00:00): " file_start_time
    if [ ! "${#file_start_time}" -eq 0 ] && [ ! "${#file_start_time}" -eq 8 ]; then
-      echo "ERROR: Invalid start time (${file_start_time}) not in hh:mm:ss format."
+      echo "ERROR: Invalid start-time (${file_start_time}) not in HH:MM:SS format."
       exit
    fi
 
    read -e -r -p "   .. ok, and truncate video at? (default ${duration_time} [ie end]): " file_end_time
    if [ ! "${#file_end_time}" -eq 0 ] && [ ! "${#file_end_time}" -eq 8 ]; then
-      echo "ERROR: Invalid end time (${file_end_time}) not in hh:mm:ss format."
+      echo "ERROR: Invalid end-time (${file_end_time}) not in HH:MM:SS format."
       exit
    fi
 
@@ -96,7 +96,7 @@ done
 cmd_list[cmd_cnt]="ffmpeg -hide_banner -loglevel panic -stats -f concat -i $ffmpeg_file_list -c copy ${file_out}"
 
 # now do any individual video modifications and join
-echo "++++++++++++++++++++"
+echo "+++++++++++++++++++++++++++++++++++++++++++++"
 echo "Preparing video operations (have patience...)"
 for cmd in "${cmd_list[@]}"
 do
