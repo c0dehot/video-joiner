e1e166692a439d70adb19f50b22c83a2a0bc1629
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-07-21 01:28:24 -0400

    Added PIP capabilities

--- video-joiner.sh
+++ video-joiner.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 # Written by Filipe Laborde, fil@rezox.com 
-# Version 0.5
-# Date: March, 2020
+# Version 0.7
+# Date: Jul, 2020
 # License: MIT
 # Use as you wish, all risk is yours.
 # Compatible with unix based systems (Mac, Ubuntu, Debian, etc.)
 #
 # Usage: ./video-joiner.sh [output.mp4] [src-dir]
 # It will prompt for the files to append together
+# If you put 2 files on the 'enter video file' line, it treats second as the PIP one (and shrinks 
+# it by 50% and puts on the top-left side)
 
+pip_scale=2
 file_out=$1
 if [ "${#1}" -eq 0 ]; then
    echo "ERROR: Please give the output filename, ex. ./video-joiner.sh output.mp4"
@@ -45,16 +48,20 @@ cmd_cnt=0
 file_idx=1
 output_resolution=""
 output_fps=""
-output_vbps="2.5"
+output_vbps=""
 while [ $file_idx -le $total_files ]; do
    echo "............................................."
 
-   read -e -r -p "Enter video file #${file_idx}: " filename
+   read -e -r -p "Enter video file (+ overlay PIP) #${file_idx}: " -a files
+   filename=${files[0]}
+   pip=${files[1]}
+   pip_position=${files[2]}
    file_ext=`echo "${filename##*.}" | tr '[:upper:]' '[:lower:]'`
-   video_info=`ffprobe -v error -select_streams v:0 -show_entries stream=width,height,codec_name,avg_frame_rate -of csv=s=x:p=0 ${filename}`
+   video_info=`ffprobe -v error -select_streams v:0 -show_entries stream=bit_rate,width,height,codec_name,avg_frame_rate -of csv=s=x:p=0 ${filename}`
    video_info=(${video_info//x/ })
    video_codec="${video_info[0]}"
    resolution="${video_info[1]}x${video_info[2]}"
+   output_vbps=`printf %.0f $(echo "${video_info[4]}/1000" | bc -l)`
    fps=`printf %.2f $(echo "${video_info[3]}+0.0001" | bc -l)`
    duration_time=`ffprobe ${filename} 2>&1 | grep -E '^ +Duration' | cut -d':' -f2- | cut -d, -f1- | cut -d'.' -f1`
    if [ -z $duration_time ]; then
@@ -62,7 +69,7 @@ while [ $file_idx -le $total_files ]; do
       exit
    fi;
 
-   echo "   * Detected: ${resolution}, ${fps} fps, ${video_codec}/${file_ext}, duration: ${duration_time}"
+   echo "   * Detected: ${resolution}, ${fps} fps, ${video_codec}/${file_ext}, vbs ${output_vbps}k, duration: ${duration_time}"
    # only ask on first file
    if [ "${file_idx}" -eq 1 ]; then
       prompt_vbps=0
@@ -91,13 +98,14 @@ while [ $file_idx -le $total_files ]; do
 
       # only bother prompting for the video-bits-per-second if one of the above changed
       if [ "$prompt_change_vbps" ]; then
-         read -e -r -p "   - Change encode-quality? (currently 3M, enter 1-3): " output_vbps
-         if [ "${#output_vbps}" -eq 0 ]; then
-            output_vbps="2.5"
-         elif [ "$output_vbps" -ge 1 ] && [ "$output_vbps" -le 4 ]; then
-            echo "   -- Ok adjusting all clips to ${output_vbps}M bit-rate"
+         read -e -r -p "   - Change encode-quality? (currently ${output_vbps}k, enter 500-3000): " output_new_vbps
+         if [ "${#output_new_vbps}" -eq 0 ]; then
+            echo "   -- Not changing vbs, staying at ${output_vbps}k"
+         elif [ "$output_new_vbps" -ge 499 ] && [ "$output_new_vbps" -le 3001 ]; then
+            output_vbps=output_new_vbps
+            echo "   -- Ok adjusting all clips to ${output_vbps}k bit-rate"
          else
-            echo "ERROR: Invalid video bit-rate (${output_vbps}): use 1.0 (for smallest file), 2 (for decent size/quality), 3.0 (for highest quality)"
+            echo "ERROR: Invalid video bit-rate (${output_new_vbps}): use 500 (for smallest file), 1500 (for decent size/quality), 3000 (for highest quality)"
             exit
          fi
       fi
@@ -116,7 +124,7 @@ while [ $file_idx -le $total_files ]; do
    fi
 
    # build temp file using extracted portion of video (or codec change)
-   if [ "${#file_start_time}" -eq 0 ] && [ "${#file_end_time}" -eq 0 ] && [ $file_ext = "mp4" ] && [ $video_codec = "h264" ] && [ $output_resolution = $resolution ]; then
+   if [ "${#file_start_time}" -eq 0 ] && [ "${#file_end_time}" -eq 0 ] && [ $file_ext = "mp4" ] && [ $video_codec = "h264" ] && [ $output_resolution = $resolution ] && [ $pip = "" ]; then
       echo "file ${filename}" >> $ffmpeg_file_list
    else
 
@@ -124,7 +132,17 @@ while [ $file_idx -le $total_files ]; do
       if [ "${#file_start_time}" -eq 8 ]; then
          cmd+="-ss ${file_start_time} "
       fi
-      cmd+="-i ${filename} "
+      if [ ${pip} ]; then
+         cmd+="-i ${pip} -i ${filename} -filter_complex '[0]scale=iw/${pip_scale}:ih/${pip_scale} [pip]; [1][pip] "
+         if [ ! ${pip_position} ] || [ $pip_position = "top" ]; then
+            cmd+="overlay=main_w-overlay_w-10:10' "
+         else
+            cmd+="overlay=main_w-overlay_w-10:main_h-overlay_h-10' "         
+         fi
+      else 
+         cmd+="-i ${filename} "
+      fi
+      
       if [ "${#file_end_time}" -eq 8 ]; then
          if [ "${#file_start_time}" -eq 0 ]; then
             file_start_time="00:00:00"
@@ -141,17 +159,19 @@ while [ $file_idx -le $total_files ]; do
          cmd+="-t ${file_duration} "
       fi
 
-      if [ $file_ext = "mp4" ] && [ $video_codec = "h264" ] && [ $output_resolution = $resolution ] && [ $output_fps = $fps ]; then
+      if [ $file_ext = "mp4" ] && [ $video_codec = "h264" ] && [ $output_resolution = $resolution ] && [ $output_fps = $fps ] &&  [ ! ${pip} ]; then
          cmd+="-c copy "
-      elif [ ! $output_resolution = $resolution ] || [ ! $output_fps = $fps ]; then
+      elif [ ${pip} ] || [ ! $output_resolution = $resolution ] || [ ! $output_fps = $fps ]; then
          # if resampling, make it smaller (3M bit-rate hardcoded, index@start)
-         cmd+="-s ${output_resolution} -r ${output_fps} -c:v libx264 -b:v ${output_vbps}M -strict -2 -movflags faststart "
+         cmd+="-s ${output_resolution} -r ${output_fps} -c:v libx264 -b:v ${output_vbps}k -strict -2 -movflags faststart "
       elif [ $video_codec = "h264" ]; then
          # since size good & codec good, keep video codec.
          cmd+="-vcodec copy "
       fi   
       tmp_filename=$filename`date +"%s"`".tmp.mp4"
+  
       cmd+="${tmp_filename}"
+
 
       cmd_list[cmd_cnt]="${cmd}"
       ((cmd_cnt++))
@@ -169,8 +189,8 @@ echo "+++++++++++++++++++++++++++++++++++++++++++++"
 echo "Preparing video operations (have patience...)"
 for cmd in "${cmd_list[@]}"
 do
-   echo " ... processing next video file... "
-   $cmd
+   echo " ... processing next video file..."
+   eval $cmd
 done
 
 # deleting temp files
@@ -186,3 +206,4 @@ echo "+++++++++++++++++++++++++++++++++++++++++++++"
 echo "COMPLETE! Written ${file_out} in ${runtime} s"
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
 echo 
+
